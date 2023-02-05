fbc569fff8b67b076c6db6b12f91f1f6560dac43
Lakshmimailz <123261375+Lakshmimailz@users.noreply.github.com>
2023-02-05 12:26:00 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CreateExpense from './create-expense'
+
+
+function App() {
+ 
+  return (
+    <>
+    <CreateExpense/>
+    
+  
+    </>
+  );
+}
+
+export default App;
